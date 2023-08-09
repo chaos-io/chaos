622baccba6dc22bb5d189f95beae693dd8244323
liankui <yuekewin@gmail.com>
2023-08-09 17:52:47 +0800

    test: update config hot load test, todo

--- config/testdata/config_test.go
+++ config/testdata/config_test.go
@@ -26,14 +26,23 @@ func TestScanFrom(t *testing.T) {
 }
 
 func TestHotLoad(t *testing.T) {
+	s := &ProjectLog{}
+	cfg := &ProjectLogConfig{}
+	if err := config.ScanFrom(cfg, "projectLogs"); err != nil {
+		t.Errorf("ScanFrom() error = %v", err)
+	}
+	s.Config = cfg
+	t.Logf("got first level %v", s.Config.Level)
+
 	ticker := time.NewTicker(time.Second)
+	timer := time.NewTimer(10 * time.Second)
 
 	for {
 		select {
 		case <-ticker.C:
-			TestScanFrom(t)
-		case <-time.After(30 * time.Second):
-			break
+			t.Logf("got level %v", s.Config.Level)
+		case <-timer.C:
+			return
 		}
 	}
 }
