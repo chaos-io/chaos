dfdd153ee21364af9f888214aa400578b0f7d7c6
Eric Yue <yuekewin@gmail.com>
2025-02-20 17:43:35 +0800

    refactor(redis): refactor the redis set testcase by spliting it into separeate methods for individual testcases

--- redis/set_test.go
+++ redis/set_test.go
@@ -1,61 +1,100 @@
 package redis
 
 import (
-	"context"
-	"fmt"
 	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Set(t *testing.T) {
-	ctx := context.Background()
-	key := "setKey"
-	member1 := "member1"
-	member2 := 2
+const (
+	member1 = "member1"
+	member2 = 2
+)
+
+func Test_SAdd(t *testing.T) {
+	key := "testSAddKey"
 
 	sAdd, err := SAdd(ctx, key, member1, member2)
 	assert.NoError(t, err)
 	assert.Equal(t, int64(2), sAdd)
-	fmt.Printf("sAdd: %v\n", sAdd)
 
+	_ = Del(ctx, key)
+}
+
+func Test_SMembers(t *testing.T) {
+	key := "testSMembersKey"
+
+	_, _ = SAdd(ctx, key, member1, member2)
 	sMembers, err := SMembers(ctx, key)
 	assert.NoError(t, err)
-	assert.Equal(t, 2, len(sMembers))
-	fmt.Printf("sMembers: %v\n", sMembers)
+	assert.Equal(t, []string{member1, strconv.Itoa(member2)}, sMembers)
 
+	_ = Del(ctx, key)
+}
+
+func Test_SCard(t *testing.T) {
+	key := "testSCardKey"
+
+	_, _ = SAdd(ctx, key, member1, member2)
 	sCard, err := SCard(ctx, key)
 	assert.NoError(t, err)
 	assert.Equal(t, int64(2), sCard)
-	fmt.Printf("sCard: %v\n", sCard)
 
+	_ = Del(ctx, key)
+}
+
+func Test_SIsMember(t *testing.T) {
+	key := "testSIsMemberKey"
+
+	_, _ = SAdd(ctx, key, member1)
 	sIsMember, err := SIsMember(ctx, key, member1)
 	assert.NoError(t, err)
 	assert.Equal(t, true, sIsMember)
-	fmt.Printf("sIsMember: %v\n", sIsMember)
 
+	sIsMember2, err := SIsMember(ctx, key, true)
+	assert.NoError(t, err)
+	assert.Equal(t, false, sIsMember2)
+
+	_ = Del(ctx, key)
+}
+
+func Test_SPop(t *testing.T) {
+	key := "testSPopKey"
+
+	_, _ = SAdd(ctx, key, member1)
+	sPop, err := SPop(ctx, key)
+	assert.NoError(t, err)
+	assert.Equal(t, member1, sPop)
+
+	sPop2, err := SPop(ctx, key)
+	assert.True(t, IsErrNil(err))
+	assert.Equal(t, "", sPop2)
+
+	_ = Del(ctx, key)
+}
+
+func Test_SRandMember(t *testing.T) {
+	key := "testSRandMemberKey"
+
+	_, _ = SAdd(ctx, key, member1)
+	sRandMember, err := SRandMember(ctx, key)
+	assert.NoError(t, err)
+	assert.Equal(t, member1, sRandMember)
+
+	_ = Del(ctx, key)
+}
+
+func Test_SRem(t *testing.T) {
+	key := "testSRemKey"
+
+	_, _ = SAdd(ctx, key, member1)
 	sRem, err := SRem(ctx, key, member1)
 	assert.NoError(t, err)
 	assert.Equal(t, int64(1), sRem)
-	fmt.Printf("sRem: %v\n", sRem)
 
-	sMembers, _ = SMembers(ctx, key)
-	fmt.Printf("sMembers2: %v\n", sMembers)
+	sIsMember, _ := SIsMember(ctx, key, member1)
+	assert.Equal(t, false, sIsMember)
 
-	sRandMember, err := SRandMember(ctx, key)
-	assert.NoError(t, err)
-	assert.Equal(t, strconv.Itoa(member2), sRandMember)
-	fmt.Printf("sRandMember: %v\n", sRandMember)
-
-	sPop, err := SPop(ctx, key)
-	assert.NoError(t, err)
-	assert.Equal(t, strconv.Itoa(member2), sPop)
-	fmt.Printf("sPop: %v\n", sPop)
-
-	sMembers, _ = SMembers(ctx, key)
-	fmt.Printf("sMembers3: %v\n", sMembers)
-
-	err = Del(ctx, key)
-	assert.NoError(t, err)
+	_ = Del(ctx, key)
 }
