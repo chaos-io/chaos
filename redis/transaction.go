8699e1c1e934712cf6f65c3c69e9faee4b92c7cf
Eric Yue <yuekewin@gmail.com>
2025-03-03 11:28:33 +0800

    feat(redis): update redis transaction Watch() and add testcases

--- redis/transaction.go
+++ redis/transaction.go
@@ -1,9 +1,18 @@
 package redis
 
 import (
+	"context"
+
 	"github.com/redis/go-redis/v9"
 )
 
+type Tx = redis.Tx
+type Pipeliner = redis.Pipeliner
+
 func Pipeline() redis.Pipeliner {
 	return GetRedis().Pipeline()
+}
+
+func Watch(ctx context.Context, fn func(*Tx) error, keys ...string) error {
+	return GetRedis().Watch(ctx, fn, keys...)
 }
