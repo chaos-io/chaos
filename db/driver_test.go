7d9d285ce3daafc6dcf17f1f4824d0855729c27f
Eric Yue <yuekewin@gmail.com>
2025-07-08 11:49:31 +0800

    misc(db): update db testcase, remove example code

--- db/driver_test.go
+++ db/driver_test.go
@@ -4,57 +4,38 @@ import (
 	"testing"
 )
 
-func testNew(t *testing.T) {
-	type args struct {
-		cfg *Config
-	}
-	tests := []struct {
-		args args
-		name string
-	}{
-		{
-			name: MysqlDriver,
-			args: args{
-				cfg: &Config{
-					Driver: MysqlDriver,
-					DSN:    "root:@tcp(127.0.0.1:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local",
-					Debug:  true,
-				},
-			},
-		},
-		{
-			name: SqliteDriver,
-			args: args{
-				cfg: &Config{
-					Driver: SqliteDriver,
-					DSN:    "test.db",
-					Debug:  true,
-				},
-			},
-		},
-		{
-			name: PostgresDriver,
-			args: args{
-				cfg: &Config{
-					Driver: PostgresDriver,
-					DSN:    "host=localhost user=eric dbname=test port=5432 sslmode=disable TimeZone=Asia/Shanghai",
-					Debug:  true,
-				},
-			},
-		},
+func test_MysqlNew(t *testing.T) {
+	cfg := &Config{
+		Driver: MysqlDriver,
+		DSN:    "root:@tcp(127.0.0.1:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local",
+		Debug:  true,
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			defer func() {
-				if r := recover(); r != nil {
-					t.Errorf("new db panic, %v", r)
-				}
-			}()
+	if db := New(cfg); db == nil {
+		t.Errorf("New() is nil")
+	}
+}
 
-			if db := New(tt.args.cfg); db == nil {
-				t.Errorf("New() is nil")
-			}
-		})
+func test_SqliteNew(t *testing.T) {
+	cfg := &Config{
+		Driver: SqliteDriver,
+		DSN:    "test.db",
+		Debug:  true,
+	}
+
+	if db := New(cfg); db == nil {
+		t.Errorf("New() is nil")
+	}
+}
+
+func test_PgsqlNew(t *testing.T) {
+	cfg := &Config{
+		Driver: PostgresDriver,
+		DSN:    "host=localhost user=eric dbname=test port=5432 sslmode=disable TimeZone=Asia/Shanghai",
+		Debug:  true,
+	}
+
+	if db := New(cfg); db == nil {
+		t.Errorf("New() is nil")
 	}
 }
