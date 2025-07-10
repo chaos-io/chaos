2c543033deaa71368d511732862fb96afdf0f95d
Eric Yue <yuekewin@gmail.com>
2025-07-10 09:55:51 +0800

    fix(config/source): fix (CWE-328): Use of weak cryptographic primitive, use crypto/sha256 replace of crypto/md5

--- config/source/changeset.go
+++ config/source/changeset.go
@@ -1,13 +1,13 @@
 package source
 
 import (
-	"crypto/md5"
+	"crypto/sha256"
 	"fmt"
 )
 
-// Sum returns the md5 checksum of the ChangeSet data.
+// Sum returns the sha256 checksum of the ChangeSet data.
 func (c *ChangeSet) Sum() string {
-	h := md5.New()
+	h := sha256.New()
 	h.Write(c.Data)
 	return fmt.Sprintf("%x", h.Sum(nil))
 }
